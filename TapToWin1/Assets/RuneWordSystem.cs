fb4c623dbc3656233e1a803c981fc9de8115d8c8
KIM TAE HOON <naxoruby@gmail.com>
2021-09-15 10:22:30 +0900

    update script can show input log

--- TapToWin1/Assets/RuneWordSystem.cs
+++ TapToWin1/Assets/RuneWordSystem.cs
@@ -24,25 +24,34 @@ public class RuneWordSystem : MonoBehaviour
 
     public void ResetRune()
     {
+        Debug.Log("Reset combination.");
         combination.text = "";
+        
     }
     public void run_func1()
     {
+        Debug.Log("Rune number 1");
         combination.text += '1';
     }
 
     public void run_func2()
     {
+
+        Debug.Log("Rune number 2");
         combination.text += '2';
     }
 
     public void run_func3()
     {
+
+        Debug.Log("Rune number 3");
         combination.text += '3';
     }
 
     public void run_func4()
     {
+
+        Debug.Log("Rune number 4");
         combination.text += '4';
     }
 }
